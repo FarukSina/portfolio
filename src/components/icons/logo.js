66ce3f63e8a583951c716014c8db2b63c59cdbb4
FarukSina <200437201@student.georgianc.on.ca>
2020-12-20 11:34:10 -0500

    GitHub pages deploy

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -12,7 +12,7 @@ const IconLogo = () => (
         <polygon
           id="Shape"
           stroke="currentColor"
-          strokeWidth="5"
+          strokeWidth="8"
           strokeLinecap="round"
           strokeLinejoin="round"
           points="39 0 0 22 0 67 39 90 78 68 78 23"
